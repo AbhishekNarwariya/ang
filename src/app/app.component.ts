e2b3b71d36452ec19c5829202aa272a6cc8e59cb
AbhishekNarwariya <narwariyaabhishek50@gmail.com>
2024-02-15 18:17:13 +0530

    adding rxjsubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, HostListener} from '@angular/core';
+import { subscribe } from 'diagnostics_channel';
+import { Subject } from 'rxjs';
 
 
 @Component({
@@ -10,7 +12,28 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'ang';
+
+  ngOnInit(){
+    const mysub = new Subject()
+
+    mysub.next(1)
+
+    
+
+
+    mysub.subscribe(res=>{
+      return console.log(res)  // yahan kuch print nahin hoga 
+    })
+
+    mysub.next(2) // yahan print hoga kyunki after subscribe print hoga
+
+
+  }
+
+  }
   
  
 
-}
+
+
+
