fdadc985e3de85134412424bc0331be2788a12c0
AbhishekNarwariya <narwariyaabhishek50@gmail.com>
2024-02-15 16:19:13 +0530

    adding inlinecss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { TempComponent } from './temp/temp.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TempComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
     
   ],
   providers: [
-    // provideClientHydration(),
+    provideClientHydration(),
 
-
-  
-    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
